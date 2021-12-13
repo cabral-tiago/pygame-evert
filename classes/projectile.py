54273e6044ea5c127a10426c6241a8f337cac811
Tiago Cabral <1412695+cabral-tiago@users.noreply.github.com>
2021-12-13 13:40:14 +0000

    [Add] Projectile class
    Adds Projectile class to serve as a base for all
    projectiles in the game.
    Refactors Fireball into a child of Projectile.

---
+++ classes/projectile.py
@@ -0,0 +1,53 @@
+from typing import Tuple
+from classes.enums import Direction
+from pygame.surface import Surface
+from pygame.rect import Rect
+
+
+class Projectile:
+    SPEED = 250
+    MAX_DISTANCE = 500
+
+    def __init__(self, surface: Surface, start: Tuple[int, int], direction: Direction) -> None:
+        self.__surface = surface
+        self.__position: Tuple[float, float] = start  # Float due to deltatime calculations
+        self.__start_position: Tuple[int, int] = start
+        self.__direction: Direction = direction
+        self.__alive = True
+
+    def __get_position(self) -> Tuple[int, int]:
+        return int(self.__position[0]), int(self.__position[1])
+
+    def get_direction(self) -> Direction:
+        return self.__direction
+
+    def get_surface(self) -> Surface:
+        return self.__surface
+
+    def get_rect(self) -> Rect:
+        return self.get_surface().get_rect(center=self.__get_position())
+
+    def is_alive(self) -> bool:
+        return self.__alive
+
+    def update(self, dt: float) -> None:
+        direction_x = 0
+        direction_y = 0
+
+        match self.__direction:
+            case Direction.RIGHT:
+                direction_x = 1
+            case Direction.LEFT:
+                direction_x = -1
+            case Direction.DOWN:
+                direction_y = 1
+            case Direction.UP:
+                direction_y = -1
+        
+        new_x = self.__position[0] + (dt * direction_x * self.SPEED)
+        new_y = self.__position[1] + (dt * direction_y * self.SPEED)
+        self.__position = (new_x, new_y)
+
+        if abs(self.__start_position[0] - self.__position[0])  > self.MAX_DISTANCE or\
+           abs(self.__start_position[1] - self.__position[1])  > self.MAX_DISTANCE:
+            self.__alive = False
--- classes/projectiles/fireball.py
+++ classes/projectiles/fireball.py
@@ -1,60 +1,26 @@
+from classes.projectile import Projectile
 from typing import Tuple
-from pygame.rect import Rect
 from pygame.surface import Surface
 from classes.enums import Direction
 import pygame
 
 
-class Fireball:
+class Fireball(Projectile):
     SPEED = 400
     MAX_DISTANCE = 600
 
     def __init__(self, start: Tuple[int, int], direction: Direction) -> None:
-        self.__surface = pygame.image.load("assets/images/fireball.png")
+        surface = pygame.image.load("assets/images/fireball.png")
 
-        self.__position: Tuple[float, float] = start
-        self.__start_position = start
-        self.__direction = direction
+        super().__init__(surface, start, direction)
 
-        self.__alive = True
-
-    def __get_position(self) -> Tuple[int, int]:
-        return int(self.__position[0]), int(self.__position[1])
-
-    def get_rect(self) -> Rect:
-        return self.get_surface().get_rect(topleft=self.__get_position())
-    
     def get_surface(self) -> Surface:
-        match self.__direction:
+        match super().get_direction():
             case Direction.UP:
-                return pygame.transform.rotate(self.__surface, 90)
+                return pygame.transform.rotate(super().get_surface(), 90)
             case Direction.DOWN:
-                return pygame.transform.rotate(self.__surface, 270)
+                return pygame.transform.rotate(super().get_surface(), 270)
             case Direction.LEFT:
-                return pygame.transform.rotate(self.__surface, 180)
+                return pygame.transform.rotate(super().get_surface(), 180)
             case _:
-                return self.__surface
-    
-    def is_alive(self) -> bool:
-        return self.__alive
-
-    def update(self, dt: float) -> None:
-        direction_x = 0
-        direction_y = 0
-        match self.__direction:
-            case Direction.RIGHT:
-                direction_x = 1
-            case Direction.LEFT:
-                direction_x = -1
-            case Direction.DOWN:
-                direction_y = 1
-            case Direction.UP:
-                direction_y = -1
-        
-        new_x = self.__position[0] + (dt * direction_x * Fireball.SPEED)
-        new_y = self.__position[1] + (dt * direction_y * Fireball.SPEED)
-        self.__position = (new_x, new_y)
-
-        if abs(self.__start_position[0] - self.__position[0])  > Fireball.MAX_DISTANCE or\
-           abs(self.__start_position[1] - self.__position[1])  > Fireball.MAX_DISTANCE:
-            self.__alive = False
+                return super().get_surface()
