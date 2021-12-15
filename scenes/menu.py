a2520133be772d552c13081ca1d3e114b074d4bf
Tiago Cabral <1412695+cabral-tiago@users.noreply.github.com>
2021-12-15 02:22:02 +0000

    [Change] Update Menu
    Fixing wrong import.
    Removing continue button.
    Updating the text on the start button

--- scenes/menu.py
+++ scenes/menu.py
@@ -2,7 +2,7 @@ from pygame.rect import Rect
 from pygame.surface import Surface
 from classes.button import Button
 from classes.scene import Scene
-from game import GameState
+from classes.enums import GameState
 import configs
 import pygame
 
@@ -45,10 +45,7 @@ class Menu(Scene):
         ref_x = int((configs.SCREEN_W / 2 ) - (Menu.MENU_BUTTON_SIZE[0] /2))
         ref_y = int(configs.SCREEN_H / 2) + 100
 
-        button_continue = Button("Continuar", Rect((ref_x, ref_y), Menu.MENU_BUTTON_SIZE), GameState.GAME_PLAYING)
-        super().add_button(button_continue)
-        
-        button_start = Button("Começar", Rect((ref_x, ref_y+80), Menu.MENU_BUTTON_SIZE), GameState.GAME_FRESH)
+        button_start = Button("Jogar", Rect((ref_x, ref_y + 80), Menu.MENU_BUTTON_SIZE), GameState.GAME_FRESH)
         super().add_button(button_start)
 
     def draw(self, screen: Surface) -> None:
